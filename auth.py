05ef793dca4cbc2b4802fc1cd9388fc7e4aa7e74
Nivedhyacodes <nivedhyas526@gmail.com>
2026-06-07 13:26:18 +0530

    Feat:Register and login

--- auth.py
+++ auth.py
@@ -1,85 +1,57 @@
+import json
+import student
+
 class User:
-    def __init__(self, username="", password=""):
-        self.username = username
-        self.password = password
-
-    def username_exists(self):
-        try:
-            with open("Users.txt" ,"r") as f:
-                for line in f:
-                    username, _ =line.strip().split(",",1)
-
-                    if username == self.username:
-                        return True
-
-            return False
-        
-        except FileNotFoundError:
-            return False
-
-    
-    def save_user(self):
-        
-        with open("Users.txt" ,'a') as f:
-            f.write(f"{self.username},{self.password}\n")
+    def roll_number_exists(self, roll_no):
             
+            with open("students.json" , 'r') as f:
+                row = json.load(f)
+                for student in row:
+                    if student["roll_no"] == roll_no:
+                        return True
     
+            return False
+                
         
+
+    def load_users(self):
+        try:
+            with open("users.json", "r") as f:
+                return json.load(f)
+        except:
+            return {}
+
+    def save_users(self, users):
+        with open("users.json", "w") as f:
+            json.dump(users, f, indent=4)
 
     def register(self):
-        
-        print("\n=== User Registration ===")
-        self.username = input("Username: ")
-        
-        if self.username_exists():
-            print("Username already exists.")
-            return
-        
-        while True:
-            
-            self.password = input("Password: ")
-            if len(self.password) < 8:
-                print("Password must be at least 8 characters long.")
-            elif not any(char.islower() for char in self.password):
-                print("Password must contain at least one lowercase letter.")
-
-            elif not any(char.isupper() for char in self.password):
-                print("Password must contain at least one uppercase letter.")
-
-            elif not any(char.isdigit() for char in self.password):
-                print("Password must contain at least one number.")
-
-            elif not any(not char.isalnum() for char in self.password):
-                print("Password must contain at least one special character.")
-
-            else:
-                print("Password accepted.")
-                break
-
-
-        self.save_user()
-        print("Registration successful.")
-
-    def login(self):
-        print("\n=== User Login ===")
-
         self.username = input("Username: ")
         self.password = input("Password: ")
 
-        try:
-            with open("Users.txt", "r") as f:
-                for line in f:
-                    file_username, file_password = line.strip().split(",", 1)
+        users = self.load_users()
 
-                    if file_username == self.username and file_password == self.password:
-                        print("Login successful.")
-                        return True
+        if self.username in users:
+            print("User already exists!")
+            return
 
-            print("Invalid username or password.")
-            return False
+        users[self.username] = self.password
+        self.save_users(users)
 
-        except FileNotFoundError:
-            print("No users found. Please register first.")
-            return False
-    
-            
+        print("Registration successful!")
+
+        roll_no = input("Enter roll number: ")
+        if not self.roll_number_exists(roll_no):
+            print("Invalid roll number")
+            return
+
+    def login(self):
+        self.username = input("Username: ")
+        self.password = input("Password: ")
+
+        users = self.load_users()
+
+        if users.get(self.username) == self.password:
+            return True
+
+        return False
